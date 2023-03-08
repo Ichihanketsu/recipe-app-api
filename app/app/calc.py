8eab065a58d539c89ebef769a05714c45bc70273
Ichihanketsu <37333451+Ichihanketsu@users.noreply.github.com>
2023-03-08 22:16:28 +0200

    feat: add basic test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """add x and y and return results."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return results."""
+    return y - x
